9c143a111f7ab96b91ea841b2fd6bf987b486278
Palman21 <53499730+Palman21@users.noreply.github.com>
2019-07-31 09:45:33 +0800

    Update index.php

--- index.php
+++ index.php
@@ -2,18 +2,16 @@
 <html>
   <head><title>Hello</title></head>
 <body style="background-color:powderblue;">
-
-  <h1><center>Hello World!</h1></center>
+<h1><center>Hello World!</h1></center>
 <p><center>Welcome to Paradise</p></center>
 <hr  border-width: 1px;>
-  <a href="http://7-themes.com/collections/hdq-paradise-wallpapers/"></a>
-  <h2> Available islands </h2>
-  <ul>
-    <li>Morocco</li>
-    <li>Paris</li>
-    <li>Madagascar</li>
-  </ul>
+<a href="http://7-themes.com/collections/hdq-paradise-wallpapers/"></a>
+<h2> Available islands </h2>
+<ul>
+ <li>Morocco</li>
+ <li>Paris</li>
+ <li>Madagascar</li>
+</ul>
   
-  
-  </body>
+</body>
 </html>
